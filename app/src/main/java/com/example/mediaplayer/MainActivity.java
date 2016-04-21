da69d14845aadc8c40f88e42d2548f5f8f783a60
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-21 00:31:25 -0400

    Add SurfaceView logic

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.mediaplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
+
+    private static final String VIDEO_URL = "http://techslides.com/demos/sample-videos/small.mp4";
+
+    private MediaPlayer mMediaPlayer;
+    private SurfaceHolder mSurfaceHolder;
+    private SurfaceView mSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSurfaceView = (SurfaceView) findViewById(R.id.a_main_surface);
+        mSurfaceHolder = mSurfaceView.getHolder();
+
+        mSurfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    mMediaPlayer = new MediaPlayer();
+                    mMediaPlayer.setDisplay(holder);
+                    mMediaPlayer.setDataSource(VIDEO_URL);
+                    mMediaPlayer.prepare();
+                    mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            Log.d(TAG, "onPrepared: ");
+                            mMediaPlayer.start();
+                        }
+                    });
+                    mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                } catch (IOException e) {
+                    Log.e(TAG, "surfaceCreated: ", e);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
     }
 }
